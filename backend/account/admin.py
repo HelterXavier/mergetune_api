1399e65fef7ad9512d0f8bf15625433cd4481e9f
Helter Xavier <helter.xavier@semeq.com>
2025-08-13 15:43:34 -0300

    feat: created admin

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,77 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirmação de Senha', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'name', 'instruments')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password1 != password2:
+            raise forms.ValidationError("As senhas não coincidem.")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label=_("Senha"))
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'name', 'instruments', 'password',
+            'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'
+        )
+
+    def clean_password(self):
+        # Retorna o valor inicial do password para evitar alteração direta
+        return self.initial['password']
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    add_form = UserCreationForm
+    form = UserChangeForm
+
+    list_display = ('email', 'name', 'is_staff', 'is_superuser', 'created_at')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Informações Pessoais'), {'fields': ('name', 'instruments')}),
+        (_('Permissões'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        (_('Datas Importantes'), {'fields': ('last_login', 'created_at')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'instruments', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+    search_fields = ('email', 'name')
+    ordering = ('-created_at',)
+    filter_horizontal = ('groups', 'user_permissions',)
